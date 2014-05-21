0339deb10a69ffb15f7c0ebaf708ec9ff13c45a0
hardy <hardy_w@yahoo.com>
2014-05-21 12:31:11 -0700

    Ring and Window Setting

---
+++ representation/RingSetting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.representation
+{
+    class RingSetting
+    {
+
+        private const int LEFT = 0;
+        private const int MID = 1;
+        private const int RIGHT = 2;
+
+
+        private int[] rings = new int[3];
+
+          
+
+
+        public RingSetting(int l, int m, int r)
+        {
+
+
+            this.rings[LEFT] = l;
+            this.rings[MID] = m;
+            this.rings[RIGHT] = r;
+
+
+        }
+
+
+        //If the ring setting is E (or 05), then offset from 'A' is +4
+        public int ringOffset(int slot)
+        {
+            return rings[slot] - 1;
+        }
+
+
+
+    }
+}
---
+++ representation/WindowSetting.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.representation
+{
+    class WindowSetting
+    {
+        private const int LEFT = 0;
+        private const int MID = 1;
+        private const int RIGHT = 2;
+
+        //the window display that our enigma operator sees
+        private char[] windows = new char[3];
+
+        //to model the mechanical idiosyncracy - double-stepping
+        private bool[] justStepped = { false, false, false };
+
+
+        public WindowSetting(char l, char m, char r)
+        {
+            this.windows[LEFT] = l;
+            this.windows[MID] = m;
+            this.windows[RIGHT] = r;
+        }
+
+        public char getWindowValue(int slot)
+        {
+            return windows[slot];
+        }
+
+        public void tick(char[] knockers)
+        {
+            bool carryToMid = false;
+            bool carryToLeft = false;
+
+            /* ***********************
+             * TICK RIGHT ROTOR
+             * ***********************/
+
+            windows[RIGHT] = (char)('A' + ((windows[RIGHT] - 'A' + 1) % 26));
+            if (windows[RIGHT] == knockers[RIGHT])
+            {
+                carryToMid = true;
+            }
+
+
+
+            /* ***********************
+             * TICK MID ROTOR (if any)
+             * ***********************/
+
+            if (carryToMid)
+            {
+                windows[MID] = (char)('A' + ((windows[MID] - 'A' + 1) % 26));
+                if (windows[MID] == knockers[MID]) carryToLeft = true;
+                justStepped[MID] = true;
+            }
+            else if (justStepped[MID])
+            {
+
+                //has the potential to double-step
+                //Double stepping occurs if MID's nextValue = knockers[MID]
+                justStepped[MID] = false;
+                char nextValue = (char)('A' + ((windows[MID] - 'A' + 1) % 26));
+                if (nextValue == knockers[MID])
+                {
+                    windows[MID] = (char)('A' + ((windows[MID] - 'A' + 1) % 26));
+                    if (windows[MID] == knockers[MID]) carryToLeft = true;
+                }
+            }
+
+
+
+            /* ***********************
+             * TICK LEFT ROTOR (if any)
+             * ***********************/
+
+            if (carryToLeft)
+            {
+                windows[LEFT] = (char)('A' + ((windows[LEFT] - 'A' + 1) % 26));
+                //justStepped[LEFT] = true; //TODO confirm whether left can also double-step 
+            }
+            else if (justStepped[LEFT])
+            {
+                justStepped[LEFT] = false;
+                windows[LEFT] = (char)('A' + ((windows[LEFT] - 'A' + 1) % 26));
+            }
+
+
+
+
+        }
+
+    }
+}
