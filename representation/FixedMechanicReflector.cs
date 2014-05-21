1fb623d696ca71fe80f55c83986c6a91822ba622
hardy <hardy_w@yahoo.com>
2014-05-21 12:30:22 -0700

    Fixed Mechanic Rotor and Reflector

---
+++ representation/FixedMechanicReflector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.representation
+{
+    public class FixedMechanicReflector
+    {
+        public static readonly FixedMechanicReflector REFLECTOR_B = new FixedMechanicReflector(
+            new string[] {
+					"AY", "BR", "CU", "DH", "EQ", "FS", "GL", "IP", "JX", "KN", "MO", "TZ", "VW"});
+
+        public static readonly FixedMechanicReflector REFLECTOR_C = new FixedMechanicReflector(
+            new string[] {
+					"AF", "BV", "CP", "DJ", "EI", "GO", "HY", "KR", "LZ", "MX", "NW", "TQ", "SU"});
+
+        public static readonly FixedMechanicReflector REFLECTOR_A = new FixedMechanicReflector(
+            new string[] {
+					"AE", "BJ", "CM", "DZ", "FL", "GY", "HX", "IV", "KW", "NR", "OQ", "PU", "ST"});
+
+
+
+        public static IEnumerable<FixedMechanicReflector> Values
+        {
+            get
+            {
+                yield return REFLECTOR_B;
+                yield return REFLECTOR_C;
+                yield return REFLECTOR_A;
+            }
+        }
+
+
+        private readonly string[] _pairing;
+
+        FixedMechanicReflector(string[] pairing)
+        {
+            this._pairing = pairing;
+        }
+	   
+        public string[] Pairing { get { return _pairing; } }
+	     
+    };
+
+
+
+
+}
+
+    
+
+    
+
---
+++ representation/FixedMechanicRotor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.representation
+{
+    class FixedMechanicRotor
+    {
+        //these are the so-called 'enum'
+        public static readonly FixedMechanicRotor ROTOR_I = new FixedMechanicRotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'R');
+        public static readonly FixedMechanicRotor ROTOR_II = new FixedMechanicRotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", 'F');
+        public static readonly FixedMechanicRotor ROTOR_III = new FixedMechanicRotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", 'W');
+        public static readonly FixedMechanicRotor ROTOR_IV = new FixedMechanicRotor("ESOVPZJAYQUIRHXLNFTGKDCMWB", 'K');
+        public static readonly FixedMechanicRotor ROTOR_V = new FixedMechanicRotor("VZBRGITYUPSDNHLXAWMJQOFECK", 'A');
+
+        //implement 'Values'
+        public static IEnumerable<FixedMechanicRotor> Values
+        {
+            get
+            {
+                yield return ROTOR_I;
+                yield return ROTOR_II;
+                yield return ROTOR_III;
+                yield return ROTOR_IV;
+                yield return ROTOR_V;
+            }
+        }
+
+        //fields private readonly
+        private readonly string _permutation;
+        private readonly char _knocker;
+
+        //constructor private
+        private FixedMechanicRotor(string perm, char knock)
+        {
+            this._permutation = perm;
+            this._knocker = knock;
+        }
+
+        //public getters
+        public string Permutation { get { return _permutation; } }
+        public char Knocker { get { return _knocker; } }
+
+
+    }
+}
